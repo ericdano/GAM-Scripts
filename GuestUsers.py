365dfb21e530e4821a4408282170944353bcdb6c
ericdano <ericdannewitz@me.com>
2020-10-16 22:38:51 -0700

    Update GuestUsers.py

--- GuestUsers.py
+++ GuestUsers.py
@@ -4,11 +4,15 @@ import smtplib, datetime, shlex, subprocess, sys, os
 import pandas as pd
 import pendulum
 
-campuses = [('ahs','edannewitz@auhsdschools.org',''),
-             ('chs','edannewitz@auhsdschools.org',''),
-             ('llhs','edannewitz@auhsdschools.org',''),
-             ('mhs','edannewitz@auhsdschools.org','')]
-docontacts = 'chenriksen@auhsdschools.org,rkahrer@auhsdschools.org'
+#Python and GAM script to rotate passwords and email to sites and district contacts
+#the information of the passwords. For use to have 5 Guest accounts available weekly for
+#sites to have guests join Zoom meetings and whatnot.
+
+campuses = [('ahs','me@school.org',''),
+             ('chs','me@school.org',''),
+             ('llhs','me@school.org',''),
+             ('mhs','me@school.org','')]
+docontacts = 'admincontact@school.org,admincontact@school.org'
 pendulum.week_starts_at(pendulum.MONDAY)
 pendulum.week_ends_at(pendulum.FRIDAY)
 today = pendulum.now().add(days=3)
@@ -23,13 +27,13 @@ for x in df.index:
     df['emailbody'][x] = "Passwords for Guest accounts this week are:\n\n"
     for i in range(5):
         password = xp.generate_xkcdpassword(mywords, delimiter="", acrostic=df['campusname'][x])
-        df['emailbody'][x] = df['emailbody'][x] + "Guest user -> " + df['campusname'][x] + "-guest" + str(i+1) + "@auhsdschools.org"  + "     Password -> " + password + "\n\n"
-        gamstring = "E:\\GAMADV-XTD3\\gam.exe update user " + df['campusname'][x] + "-guest" + str(i+1) + "@auhsdschools.org password " + password
+        df['emailbody'][x] = df['emailbody'][x] + "Guest user -> " + df['campusname'][x] + "-guest" + str(i+1) + "@school.org"  + "     Password -> " + password + "\n\n"
+        gamstring = "E:\\GAMADV-XTD3\\gam.exe update user " + df['campusname'][x] + "-guest" + str(i+1) + "@school.org password " + password
         p = subprocess.Popen(["powershell.exe",gamstring],stdout=sys.stdout)
         p.communicate()
     s = smtplib.SMTP('10.99.0.202')
     msg['Subject'] = "Guest Account Passwords for " + df['campusname'][x].upper() + " " + theweekof
-    msg['From'] = 'donotreply@auhsdschools.org'
+    msg['From'] = 'donotreply@school.org'
     msg['To'] = str(df['contacts'][x] + "," + docontacts)
     msg.set_content(df['emailbody'][x])
     s.send_message(msg)
